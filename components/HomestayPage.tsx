11e43264f3a08b23e7305cac18025ddb3fd1e46c
avishkaindula <avishkainduladev@gmail.com>
2025-06-07 17:45:08 +0530

    feat: Implement HomestayPage with mobile profile and bottom tab navigation

---
+++ components/HomestayPage.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from "react";
+import { StatusBar, Platform } from "react-native";
+import { Box } from "./ui/box";
+import MobileBottomTabs from "./MobileBottomTabs";
+import MobileModeChangeButton from "./MobileModeChangeButton";
+import {
+  Plus,
+  Home,
+  MessageCircle,
+  User,
+  SlidersHorizontal,
+} from "lucide-react-native";
+import MobileProfilePage from "./MobileProfilePage";
+import Explorepage from "./ExplorePage";
+import MobileSidebarActionsheet from "./MobileSidebarActionsheet";
+
+const bottomTabs = [
+  {
+    icon: Home,
+    label: "Home",
+  },
+  {
+    icon: SlidersHorizontal,
+    label: "Filter",
+  },
+  {
+    icon: Plus,
+    label: "Listing",
+  },
+  {
+    icon: MessageCircle,
+    label: "Inbox",
+    disabled: true,
+  },
+  {
+    icon: User,
+    label: "Profile",
+  },
+];
+
+const HomestayPage = () => {
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      document.body.style.overflow = "hidden";
+      document.body.style.height = "100%";
+    }
+  }, []);
+
+  const [activeTab, setActiveTab] = React.useState("Home");
+
+  return (
+    <>
+      <Box className="flex-1 bg-white dark:bg-background-dark-950">
+        <StatusBar />
+
+        <Box className="flex-1">
+          <MobileProfilePage isActive={activeTab === "Profile"} />
+
+          <Explorepage setActiveTab={setActiveTab} activeTab={activeTab} />
+
+          <MobileModeChangeButton />
+        </Box>
+        {/* mobile bottom tabs */}
+        <Box className="h-[72px] items-center w-full md:hidden border-t border-border-light-50 dark:border-border-dark-900">
+          <MobileBottomTabs
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+            bottomTabs={bottomTabs}
+          />
+        </Box>
+      </Box>
+      {/* )} */}
+    </>
+  );
+};
+export default HomestayPage;
