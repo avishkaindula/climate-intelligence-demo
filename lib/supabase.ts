717fd5bf0ce5f8ea355c49a66e63a7efb33ea395
avishkaindula <avishkainduladev@gmail.com>
2025-06-24 10:11:11 +0530

    feat: add environment variables for Supabase configuration and update .gitignore

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { Platform } from "react-native";
 
-const supabaseUrl = "https://vwklkjlguperbfxmblbg.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3a2xramxndXBlcmJmeG1ibGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMTUyODIsImV4cCI6MjA2NTc5MTI4Mn0.HP4kHBB3Vjc50A7xOgyQLuzcT2-H5-zcAEcPcLcchU4";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 // Custom storage that works on both native and web
 const customStorage = {
